68dc6cc83304a9e2c737760f2f93595694cb23ce
Kazachuk Nailya <yakimovanailya@yandex.ru>
2024-03-24 15:07:31 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/<название-вашего-репозитория>/'
+    ? '/play-form/'
     : '/',
   transpileDependencies: true,
   chainWebpack: config => {
